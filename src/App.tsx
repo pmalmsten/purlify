9d7018689f11802c0eef57dfed55f542eaab5d71
Paul Malmsten <pmalmsten@gmail.com>
2023-07-04 17:07:50 -0700

    Initial commit: decoding of package URL works

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import {
+    Box,
+    Card,
+    CardContent,
+    CardHeader,
+    Container,
+    CssBaseline, List, ListItem, TextField,
+    Typography
+} from "@mui/material";
+import {PackageURL} from "packageurl-js"
+import Stack from "@mui/material/Stack/Stack";
+
+function DecodePackageURLCard() {
+    const [urlToDecode, setUrlToDecode] = useState<string>("")
+
+    let decodedPurl: PackageURL | undefined = undefined;
+    let decodedUrlError = false;
+
+    if (urlToDecode.length > 0) {
+        try {
+            decodedPurl = PackageURL.fromString(urlToDecode);
+        } catch (error) {
+            decodedUrlError = true
+        }
+    }
+
+    return <Card sx={{marginBottom: 4}}>
+        <CardHeader
+            title="Decode Package URL"
+            subheader="Decompose a Package URL into its components"
+            sx={{
+                backgroundColor: (theme) => theme.palette.primary.main,
+                color: (theme) => theme.palette.primary.contrastText
+            }}
+            subheaderTypographyProps={{
+                color: (theme) => theme.palette.primary.contrastText
+            }}
+        />
+        <CardContent>
+            <TextField
+                variant="filled"
+                label="Package URL, e.g. pkg:npm/lodash@4.17.21"
+                fullWidth
+                onChange={(event) => setUrlToDecode(event.target.value)}
+                value={urlToDecode}
+                error={decodedUrlError}
+                helperText={decodedUrlError ? "Invalid Package URL" : ""}
+            />
+            {decodedPurl !== undefined &&
+                <Stack>
+                    <Box>
+                        <Typography sx={{
+                            fontWeight: 'bold',
+                            display: 'inline'
+                        }}>Type: </Typography>
+                        {decodedPurl.type}
+                    </Box>
+                    <Box>
+                        <Typography sx={{
+                            fontWeight: 'bold',
+                            display: 'inline'
+                        }}>Namespace: </Typography>
+                        {decodedPurl.namespace}
+                    </Box>
+                    <Box>
+                        <Typography sx={{
+                            fontWeight: 'bold',
+                            display: 'inline'
+                        }}>Name: </Typography>
+                        {decodedPurl.name}
+                    </Box>
+                    {decodedPurl.version &&
+                        <Box>
+                            <Typography sx={{
+                                fontWeight: 'bold',
+                                display: 'inline'
+                            }}>Version: </Typography>
+                            {decodedPurl.version}
+                        </Box>}
+                    {decodedPurl.subpath &&
+                        <Box>
+                            <Typography sx={{
+                                fontWeight: 'bold',
+                                display: 'inline'
+                            }}>Subpath: </Typography>
+                            {decodedPurl.subpath}
+                        </Box>}
+                    {decodedPurl?.qualifiers && <Box>
+                        <Typography sx={{fontWeight: 'bold'}}>Qualifiers:</Typography>
+                        <List>
+                            {Object.keys(decodedPurl.qualifiers).map(key =>
+                                <ListItem>
+                                    {key}: {decodedPurl?.qualifiers && decodedPurl.qualifiers[key]}
+                                </ListItem>)}
+                        </List>
+                    </Box>}
+                </Stack>}
+        </CardContent>
+    </Card>
+}
+
+function GenerateFromRegistryURLCard() {
+    return <Card sx={{marginBottom: 4}}>
+        <CardHeader
+            title="Create Package URL From Package Registry URL"
+            subheader="Create a Package URL from a package registry URL (e.g. https://www.npmjs.com/package/lodash/v/4.17.21)"
+            sx={{
+                backgroundColor: (theme) => theme.palette.primary.main,
+                color: (theme) => theme.palette.primary.contrastText
+            }}
+        />
+        <CardContent>
+            <TextField variant="filled"
+                       label="Package Registry URL, e.g. https://www.npmjs.com/package/lodash/v/4.17.21"
+                       fullWidth/>
+        </CardContent>
+    </Card>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <CssBaseline/>
+            <Container disableGutters maxWidth="lg" component="main" sx={{pt: 8, pb: 6}}>
+                <DecodePackageURLCard/>
+                {/*<GenerateFromRegistryURLCard/>*/}
+            </Container>
+        </>
+    );
 }
 
 export default App;
