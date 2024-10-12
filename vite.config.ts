c85c3476b001e3a4b6e10ad234a90de750607cf9
Paul Malmsten <pmalmsten@gmail.com>
2024-10-12 20:53:15 +0000

    Fix tests by migrating to Vitest

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    // globals: true,
+    environment: "jsdom"
+  },
+})
